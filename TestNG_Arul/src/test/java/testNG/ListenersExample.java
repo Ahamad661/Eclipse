ce0b400990f3eac94fb0d8b265c0037d224419d8
ahama <ahama@AHAMAD>
2023-01-01 23:18:44 +0530

    Initial Commit

--- TestNG_Arul/src/test/java/testNG/ListenersExample.java
+++ TestNG_Arul/src/test/java/testNG/ListenersExample.java
@@ -1,6 +1,30 @@
 package testNG;
 
-public class ListenersExample {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
-	
+public class ListenersExample implements ITestListener {
+
+	public void onTestStart(ITestResult result) {
+		System.out.println("Test Case is going to execute");
+	}
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test case is passed");
+	}
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test case is failed");
+	}
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("Test case is skipped");
+	}
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+	}
+	public void onStart(ITestContext context) {
+		System.out.println("Before Everyting");
+	}
+	public void onFinish(ITestContext context) {
+		System.out.println("After Everything");
+	}
 }
---
+++ TestNG_Arul/src/test/java/testNG/ListenersExecute.java
@@ -0,0 +1,11 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class ListenersExecute {
+
+	@Test
+	public void firstTestCase() {
+		System.out.println("This is the first test case");
+	}
+}
